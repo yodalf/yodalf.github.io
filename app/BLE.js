1ec0f57caa42cc842c62a08c845d4f92adfa3093
Real Ouellet <real@realo.ca>
2024-08-25 08:30:52 -0400

    WIP

---
+++ app/BLE.js
@@ -0,0 +1,14 @@
+const connectButton = document.getElementById("controlButton");
+const deviceName = document.getElementById("deviceNameInput");
+const status = document.getElementById("connectionStatus");
+
+connectButton.addEventListener("click", BLEManager);
+
+async function BLEManager()
+{
+
+    const device = await navigator.bluetooth.requestDevice( [ acceptAllDevices:true ] );
+
+}
+
+
