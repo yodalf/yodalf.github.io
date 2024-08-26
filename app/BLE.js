4aac36fce199478fb286b6da19421f68b002d014
Real Ouellet <real@realo.ca>
2024-08-26 05:41:28 -0400

    WIP

--- app/BLE.js
+++ app/BLE.js
@@ -33,6 +33,7 @@ async function BLEConnectionHandler()
     if (device) {
         if (device.gatt.connected) {
             device.gatt.disconnect();
+            device.gatt.forget();
         }
         else
         {
