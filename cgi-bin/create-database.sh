a788f3638a65c8334f1a1881a05dac1ccb8f05ab
Real Ouellet <real.r.ouellet@ca.abb.com>
2024-12-05 11:36:52 +0000

    WIP Create user, worker onboarding

--- cgi-bin/create-database.sh
+++ cgi-bin/create-database.sh
@@ -2,7 +2,7 @@
 
 rm -f database.db
 
-sqlite3 database.db "CREATE TABLE users ( id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT NOT NULL UNIQUE, pwdhash TEXT, cert TEXT, role TEXT);"
+sqlite3 database.db "CREATE TABLE users ( id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT NOT NULL UNIQUE, usercert TEXT, userkey TEXT, pwdhash TEXT, cert TEXT, role TEXT);"
 sqlite3 database.db "CREATE TABLE mdevs ( id INTEGER PRIMARY KEY AUTOINCREMENT, certserial NOT NULL UNIQUE, cert TEXT);"
 sqlite3 database.db "CREATE TABLE sessions ( id INTEGER PRIMARY KEY AUTOINCREMENT, mdevserial NOT NULL, username TEXT, symkey TEXT, jwt TEXT);"
                 
